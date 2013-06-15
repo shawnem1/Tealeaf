4faf630a87ec9d66dcd2277d265f285b5f1d36ff
Shawne Maynard <shawnem1@gmail.com>
2013-06-14 19:33:57 -0700

    Changed last line to conform with convention.

--- ninety_nine_bottles_of_beer.rb
+++ ninety_nine_bottles_of_beer.rb
@@ -7,5 +7,5 @@ while count > 1
   count = count - 1
 end
 
-puts 'No more bottles of beer on the wall, no more bottles of beer. 
-Go to the store and buy some more, 99 bottles of beer on the wall.'
+puts 'No more bottles of beer on the wall, no more bottles of beer.' 
+puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
